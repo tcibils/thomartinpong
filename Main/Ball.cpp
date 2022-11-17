506e9ae1fbb224c1f5a2926d48fa124704935831
Martin <martin1.cibils@gmail.com>
2022-11-17 15:48:25 +0100

    Adding ball and pointonmatrix

---
+++ Main/Ball.cpp
@@ -0,0 +1,18 @@
+#include "pointOnMatrix.cpp"
+
+class Ball {       // The class
+  public:             // Access specifier
+    pointOnMatrix pos;        
+    pointOnMatrix size;        
+    pointOnMatrix speed;        
+    pointOnMatrix acc;       
+    Ball(pointOnMatrix position, pointOnMatrix sizes, pointOnMatrix speed_, pointOnMatrix acceleration){
+      pos = position;
+      size = sizes;
+      speed = speed_;
+      acc = acceleration;
+    }
+    
+};
+
+
